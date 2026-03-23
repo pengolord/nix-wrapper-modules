2f218088a996119ea31c1db188a97d56d46174bb
Rachit Kumar Verma <155641117+rachitvrma@users.noreply.github.com>
2026-03-23 18:48:38 +0000

    feat(wrapperModules.fastfetch): init (#365)
    add wrapper module for the
    [fastfetch](https://github.com/fastfetch-cli/fastfetch)
    system information tool

---
+++ wrapperModules/f/fastfetch/module.nix
@@ -0,0 +1,33 @@
+{
+  wlib,
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+{
+  imports = [ wlib.modules.default ];
+  options = {
+    settings = lib.mkOption {
+      type = lib.types.json;
+      default = { };
+      description = ''
+        Configuration passed to fastfetch using `--config` flag
+        See <https://github.com/fastfetch-cli/fastfetch/wiki/Json-Schema>
+        for the documentation.
+      '';
+    };
+  };
+  config = {
+    package = lib.mkDefault pkgs.fastfetch;
+    constructFiles.generatedConfig = {
+      content = builtins.toJSON config.settings;
+      relPath = "${config.binName}-settings.json";
+    };
+    flags = {
+      "--config" = config.constructFiles.generatedConfig.path;
+    };
+
+    meta.maintainers = [ wlib.maintainers.rachitvrma ];
+  };
+}
